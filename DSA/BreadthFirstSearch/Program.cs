49f83e1084c7d5fba2ca171efa10261a59fe990c
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-03-15 21:53:50 -0300

    Breadth First Search (BFS)

---
+++ DSA/BreadthFirstSearch/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace BreadthFirstSearch
+{
+    internal class Program
+    {
+        private static Dictionary<string, string[]> _graph = new();
+
+        static void Main(string[] args)
+        {
+            _graph.Add("you", ["alice", "bob", "claire"]);
+            _graph.Add("bob", ["anuj", "peggy"]);
+            _graph.Add("alice", ["peggy"]);
+            _graph.Add("claire", ["thom", "jonny"]);
+            _graph.Add("anuj", []);
+            _graph.Add("peggy", []);
+            _graph.Add("thom", []);
+            _graph.Add("jonny", []);
+
+            Search();
+        }
+
+        static bool PersonIsSeller(string name) => name.EndsWith('m');
+
+        static bool Search()
+        {
+            var queue = new Queue<string>(_graph["you"]);
+            var visited = new HashSet<string>();
+
+            while (queue.Count > 0)
+            {
+                var person = queue.Dequeue();
+
+                if (!visited.Contains(person))
+                {
+                    if (PersonIsSeller(person))
+                    {
+                        Console.WriteLine($"{person} is a mango seller!");
+                        return true;
+                    }
+
+                    foreach (var neighbor in _graph[person])
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+
+                    visited.Add(person);
+                }
+            }
+
+            return false;
+        }
+    }
+}
